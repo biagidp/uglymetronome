d5562e8ad17dadaad0bda64b86c7de94381298cc
David Biagi <biagidp@gmail.com>
2023-09-24 16:19:27 -0500

    Adds the demake

---
+++ HTMLJavaScript/README.md
@@ -0,0 +1,3 @@
+# HTML/JavaScript
+
+I lied, this implementation is four files because we still need the sound file. The HTML is very clean, all the logic is in a single JavaScript file and the `style.css` file is small enough to remain easy to parse at a glance.
---
+++ HTMLJavaScript/index.js
@@ -0,0 +1,96 @@
+const measure = document.getElementById("measure");
+let timeout;
+function startLoop(measure) {
+  const markOn = Array.from(document.getElementsByName("mark")).find(
+    (e) => e.checked,
+  ).value;
+  const intervals = getIntervals(measure, markOn);
+  continueMeasure(intervals);
+}
+
+function resetLoop(measure) {
+  clearTimeout(timeout);
+  const markOn = Array.from(document.getElementsByName("mark")).find(
+    (e) => e.checked,
+  ).value;
+  const intervals = getIntervals(measure, markOn);
+  intervals.map((b) => b.classList.remove("active"));
+}
+
+function continueMeasure(intervals) {
+  const toksPerMinute = getToksPerMinute();
+  const activeInterval = getActiveInterval(intervals);
+  const tok = new Audio("./tok.mp3");
+  tok.play();
+  // delete tok;
+  timeout = setTimeout(() => {
+    progressInterval(intervals, activeInterval);
+  }, 60000 / toksPerMinute);
+}
+
+function progressInterval(intervals, activeInterval) {
+  const progressToIndex = intervals.indexOf(activeInterval) + 1;
+  const nextInterval =
+    progressToIndex >= intervals.length
+      ? intervals[0]
+      : intervals[progressToIndex];
+  activeInterval.classList.remove("active");
+  nextInterval.classList.add("active");
+  continueMeasure(intervals);
+}
+
+function getActiveInterval(intervals) {
+  const activeInterval = intervals.find((interval) =>
+    interval.classList.contains("active"),
+  );
+  if (activeInterval) return activeInterval;
+
+  const defaultInterval = intervals[0];
+  defaultInterval.classList.add("active");
+  return defaultInterval;
+}
+
+function getIntervals(measure, markOn) {
+  switch (markOn) {
+    case "1":
+      return Array.from(measure.getElementsByClassName("beat"));
+    case "1/8":
+      return Array.from(measure.getElementsByClassName("eighth"));
+    case "1/16":
+      const beats = Array.from(measure.getElementsByClassName("beat"));
+      const orderedSixteenths = [];
+      beats.map((beat) => {
+        const sixteenths = Array.from(beat.getElementsByClassName("sixteenth"));
+        const sorted = sixteenths.sort((a, b) => {
+          if (a.dataset["sixteenths"] === b.dataset["sixteenths"]) return 0;
+          if (a.dataset["sixteenths"] < b.dataset["sixteenths"]) return -1;
+          if (a.dataset["sixteenths"] > b.dataset["sixteenths"]) return 1;
+        });
+        orderedSixteenths.push(sorted);
+      });
+      return orderedSixteenths.flat();
+  }
+}
+
+function getToksPerMinute() {
+  const markOn = Array.from(document.getElementsByName("mark")).find(
+    (e) => e.checked,
+  ).value;
+  const bpm = parseInt(document.getElementById("bpm").value);
+  switch (markOn) {
+    case "1":
+      return bpm;
+    case "1/8":
+      return bpm * 2;
+    case "1/16":
+      return bpm * 4;
+  }
+}
+
+document
+  .getElementById("start-button")
+  .addEventListener("click", () => startLoop(measure));
+
+document
+  .getElementById("stop-button")
+  .addEventListener("click", () => resetLoop(measure));
